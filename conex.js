a53f95b206ad26c6e15cc33e6765d1b95e7f75bd
mau77A7 <107590968+mau77A7@users.noreply.github.com>
2023-02-13 13:51:23 -0300

    Update conex.js

--- conex.js
+++ conex.js
@@ -1,9 +1,9 @@
-/**const spreadsheetID = '1e3iq9tLZRrQxgi7tjeZHg6PQPHXqy0k1rNNv4mJe74E'
+const spreadsheetID = '1e3iq9tLZRrQxgi7tjeZHg6PQPHXqy0k1rNNv4mJe74E'
 const url = `https://docs.google.com/spreadsheets/d/${spreadsheetID}/gviz/tq?tqx=out:json`
 let data = fetch(url)
 .then(res => res.text())
 .then(text => JSON.parse(text.substr(47).slice(0, -2)))
-console.log(parser(data)); **/        
+//console.log(parser(data));        
 
 
 function parser({table}){
