dabf3d0bc0b73de020fa843bab35b6cf638a2cce
mau77A7 <107590968+mau77A7@users.noreply.github.com>
2023-02-13 13:47:35 -0300

    Update conex.js

--- conex.js
+++ conex.js
@@ -1,9 +1,9 @@
-const spreadsheetID = '1e3iq9tLZRrQxgi7tjeZHg6PQPHXqy0k1rNNv4mJe74E'
+/**const spreadsheetID = '1e3iq9tLZRrQxgi7tjeZHg6PQPHXqy0k1rNNv4mJe74E'
 const url = `https://docs.google.com/spreadsheets/d/${spreadsheetID}/gviz/tq?tqx=out:json`
 let data = fetch(url)
 .then(res => res.text())
 .then(text => JSON.parse(text.substr(47).slice(0, -2)))
-console.log(parser(data));         
+console.log(parser(data)); **/        
 
 
 function parser({table}){
@@ -11,8 +11,7 @@ function parser({table}){
         .map(row => [ ...table.cols.map((col, index) => ({[col.label]: row.c[index].v,}))])
         .map(data => data.reduce((acc, value) =>  ({...acc, ...value}), {}))
     }
-
-/** function listar() {
+function listar() {
      const spreadsheetID = '1e3iq9tLZRrQxgi7tjeZHg6PQPHXqy0k1rNNv4mJe74E'
     const url = `https://docs.google.com/spreadsheets/d/${spreadsheetID}/gviz/tq?tqx=out:json`
     let data =  fetch(url)
@@ -21,5 +20,5 @@ function parser({table}){
 
     console.log(parser(data));
  }
-**/
+
  
