acd3fe306bc7f443ac91429867082e1181661660
mau77A7 <107590968+mau77A7@users.noreply.github.com>
2023-02-13 13:46:39 -0300

    Create conex.js

---
+++ conex.js
@@ -0,0 +1,25 @@
+const spreadsheetID = '1e3iq9tLZRrQxgi7tjeZHg6PQPHXqy0k1rNNv4mJe74E'
+const url = `https://docs.google.com/spreadsheets/d/${spreadsheetID}/gviz/tq?tqx=out:json`
+let data = fetch(url)
+.then(res => res.text())
+.then(text => JSON.parse(text.substr(47).slice(0, -2)))
+console.log(parser(data));         
+
+
+function parser({table}){
+        return table.rows
+        .map(row => [ ...table.cols.map((col, index) => ({[col.label]: row.c[index].v,}))])
+        .map(data => data.reduce((acc, value) =>  ({...acc, ...value}), {}))
+    }
+
+/** function listar() {
+     const spreadsheetID = '1e3iq9tLZRrQxgi7tjeZHg6PQPHXqy0k1rNNv4mJe74E'
+    const url = `https://docs.google.com/spreadsheets/d/${spreadsheetID}/gviz/tq?tqx=out:json`
+    let data =  fetch(url)
+    .then(res => res.text())
+    .then(text => JSON.parse(text.substr(47).slice(0, -2)))
+
+    console.log(parser(data));
+ }
+**/
+ 
